7a9c4089d3a2361b85af330c3b6d7a4401c66523
Barbecue23 <sunsanasupphong_j@silpakorn.com>
2024-03-03 16:14:10 +0700

    update

---
+++ lib/models/country.dart
@@ -0,0 +1,14 @@
+// POJO (Plain Old Java Object)
+class Country {
+  final String name;
+  final String capital;
+  final int population;
+  final String flag;
+
+  Country({
+    required this.name,
+    required this.capital,
+    required this.population,
+    required this.flag,
+  });
+}
