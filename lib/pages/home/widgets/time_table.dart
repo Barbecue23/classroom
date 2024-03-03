89619b0263a79ffc6eb977b42ad6c90ff251ea30
Barbecue23 <sunsanasupphong_j@silpakorn.com>
2024-03-03 11:30:51 +0700

    update

--- lib/pages/home/widgets/time_table.dart
+++ lib/pages/home/widgets/time_table.dart
@@ -5,8 +5,20 @@ class TimeTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('TimeTable'),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          for (var i = 0; i < 20; i++)
+            ListTile(
+              title: Text('TimeTable ${i + 1}'),
+              subtitle: Text('View your timetable'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print('View timetable ${i + 1}');
+              },
+            ),
+        ],
+      ),
     );
   }
 }
